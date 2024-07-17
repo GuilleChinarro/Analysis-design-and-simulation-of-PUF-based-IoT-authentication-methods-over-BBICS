1652409ef53df92ac9a4a9a97ea30de158ac88c1
GuilleChinarro <134952980+GuilleChinarro@users.noreply.github.com>
2024-07-17 16:47:53 +0200

    Update blockchain_validation.py

--- Simulator/blockchain_validation.py
+++ Simulator/blockchain_validation.py
@@ -3,18 +3,7 @@ import json
 import time
 
 class Block:
-    """
-    Clase que representa un bloque en la cadena de bloques.
-    """
-
     def __init__(self, index, timestamp, data, previous_hash=''):
-        """
-        Inicializa un bloque con los siguientes parámetros:
-        - index: El índice del bloque en la cadena.
-        - timestamp: La marca de tiempo del bloque.
-        - data: datos del bloque (puede ser cualquier información relevante).
-        - previous_hash: El hash del bloque anterior en la cadena (opcional).
-        """
         self.index = index
         self.timestamp = timestamp
         self.data = data
@@ -23,9 +12,6 @@ class Block:
         self.hash = self.calculate_hash()
 
     def calculate_hash(self):
-        """
-        Calcula el hash del bloque.
-        """
         block_data = {
             "index": self.index,
             "timestamp": self.timestamp,
@@ -33,32 +19,22 @@ class Block:
             "nonce": self.nonce,
             "data": self.data
         }
-        
-        # Serializar los datos del bloque y calcular el hash
         block_string = json.dumps(block_data, sort_keys=True).encode()
         return sha256(block_string).hexdigest()
 
 class SmartContract:
-    """
-    Clase que representa un smart contract para almacenar parejas challenge-response.
-    """
-
     def __init__(self):
-        """Inicializa el contrato inteligente con un diccionario vacío de desafíos y respuestas."""
         self.challenge_response_pairs = {}
         self.state = {}
 
     def store_puf(self, identifier, challenges_responses_dict, estado):
-        """Almacena las parejas challenge-response validadas en el contrato inteligente."""
         self.challenge_response_pairs[identifier] = challenges_responses_dict
         self.state[identifier] = {"estado": estado}
 
     def get_challenges(self, identifier):
-        """Devuelve todos los desafíos almacenados para un identificador dado."""
         return list(self.challenge_response_pairs.get(identifier, {}).keys())
 
     def get_response_by_index(self, identifier, index):
-        """Devuelve la respuesta correspondiente a un desafío específico en un identificador dado."""
         challenges = self.get_challenges(identifier)
         if 0 <= index < len(challenges):
             return self.challenge_response_pairs[identifier][challenges[index]]
@@ -66,11 +42,9 @@ class SmartContract:
             return None
         
     def get_state(self, identifier):
-        """Devuelve el estado almacenado para un identificador dado."""
         return self.state.get(identifier, {})
 
     def set_state(self, identifier, new_state):
-        """Actualiza el estado para un identificador dado."""
         if identifier in self.state:
             self.state[identifier]["estado"] = new_state
  
@@ -88,18 +62,12 @@ class Blockchain:
         self.current_hashes = 0
 
     def create_genesis_block(self):
-        """Crea el bloque génesis."""
         return Block(0, time.time(), "Genesis Block", "0")
 
     def get_latest_block(self):
-        """Obtiene el último bloque de la cadena."""
         return self.chain[-1]
 
     def add_block(self, new_block):
-        """
-        Añade un nuevo bloque a la cadena después de minarlo.
-        """
-        
         new_block.previous_hash = self.get_latest_block().hash
         self.current_hashes = 0
         current_validation_time = 0
@@ -116,9 +84,6 @@ class Blockchain:
         self.calculate_hash_rate()
 
     def mine_block(self, block):
-        """
-        Mina el bloque (calcula el hash que cumple con la dificultad).
-        """
         while block.hash[:self.difficulty] != "0" * self.difficulty:
             block.nonce += 1
             block.hash = block.calculate_hash()
@@ -128,8 +93,6 @@ class Blockchain:
         return block.hash
 
     def is_chain_valid(self):
-        """Verifica la validez de la cadena de bloques."""
-
         for i in range(1, len(self.chain)):
             current_block = self.chain[i]
             previous_block = self.chain[i - 1]
@@ -141,7 +104,6 @@ class Blockchain:
         return True
 
     def add_smart_contract(self, smart_contract):
-        """Añade un contrato inteligente a la cadena de bloques."""
         self.pending_smart_contracts.append(smart_contract)
 
         def convert_keys_to_strings(d):
@@ -152,11 +114,9 @@ class Blockchain:
                 return [convert_keys_to_strings(i) for i in d]
             else:
                 return d
-        
-        # Convertir las claves del diccionario a cadenas
+    
         converted_dict = convert_keys_to_strings(smart_contract.challenge_response_pairs)
         
-        # Crear una copia del contrato inteligente con las claves convertidas
         smart_contract_data = {
             'challenge_response_pairs': converted_dict,
             'state': smart_contract.state
@@ -170,7 +130,6 @@ class Blockchain:
 
 
     def get_latest_smart_contract(self):
-        """Obtiene y deserializa el último contrato inteligente añadido a la cadena."""
         if not self.chain:
             return None
 
@@ -178,7 +137,6 @@ class Blockchain:
         smart_contract_data = json.loads(latest_block.data)
 
         def convert_keys_back(d):
-            """Convierte las claves de los diccionarios a tuplas si es necesario."""
             if isinstance(d, dict):
                 new_dict = {}
                 for k, v in d.items():
@@ -193,7 +151,6 @@ class Blockchain:
             else:
                 return d
 
-        # Convertir las claves del diccionario de vuelta a su formato original
         smart_contract_data['challenge_response_pairs'] = convert_keys_back(smart_contract_data['challenge_response_pairs'])
 
         smart_contract = SmartContract()
@@ -227,13 +184,13 @@ class Blockchain:
 def validate_blockchain(blockchain):
     print()
     if blockchain.is_chain_valid():
-        print("Validación de los bloques correcta.")
+        print("Validation of the blocks: successful.")
     else:
-        print("Error en la validación de los bloques.")
+        print("Error in the validation of the blocks.")
 
     avg_time = blockchain.get_avg_block_validation_time()
     hash_rate = blockchain.calculate_hash_rate()
-    print(f"Tiempo promedio de validación de bloques: {avg_time:.6f} segundos")
-    print(f"Tasa de hash: {hash_rate:.6f} hashes/segundo")
+    print(f"Average block validation time: {avg_time:.6f} seconds")
+    print(f"Hash rate: {hash_rate:.6f} hashes/seconds")
 
     blockchain.total_validation_time += avg_time
